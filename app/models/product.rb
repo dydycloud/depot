7f7e3ba39e46a48fe082e297ddb4b2c1ed21e32f
dydycloud <sandy.pierre97@gmail.com>
2012-04-24 15:01:15 +0200

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,13 @@
 class Product < ActiveRecord::Base
+	#default_scope
+	default_scope :order => 'title'
+
+	#validation 
   attr_accessible :description, :image_url, :price, :title
 
   validates :title, :description, :image_url, :presence => true
+
+  validates_length_of :title, :minimum => 3, :too_short => "please enter at least 3 characters"
 
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
 
